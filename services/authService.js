8fd2d2eebf1874006dd45175ba07ff033d0fc22d
itay <sasmoga1098@gmail.com>
2025-01-10 12:32:40 +0200

    update dependencies and add loginUser function to authService

--- services/authService.js
+++ services/authService.js
@@ -6,5 +6,13 @@ export async function registerUser(userData) {
   const API_URL = `${REACT_APP_SERVER_DOMAIN}/posts`; // Example API endpoint
 
   const response = await axios.post(API_URL, userData);
-  return response.data; 
+  return response.data;
+}
+
+// Example function to handle login:
+export async function loginUser(userData) {
+  const API_URL = `${REACT_APP_SERVER_DOMAIN}/login`; // Example API endpoint
+
+  const response = await axios.post(API_URL, userData);
+  return response.data;
 }
